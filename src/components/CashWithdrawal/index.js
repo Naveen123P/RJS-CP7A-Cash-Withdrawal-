26860ded5c2a7a015dfa1bf00230f66e6166852b
Naveen123P <125892820+Naveen123P@users.noreply.github.com>
2023-11-19 08:07:19 +0530

    Update index.js

--- src/components/CashWithdrawal/index.js
+++ src/components/CashWithdrawal/index.js
@@ -1,45 +1,72 @@
 // Write your code here
+import {Component} from 'react'
 import DenominationItem from '../DenominationItem'
 import './index.css'
 
-const CashWithdrawal = props => {
-  const {denominationsList} = props
-  const {id, value} = denominationsList
-  let balance = 2000
+const initialDenominationsList = [
+  {
+    id: 1,
+    value: 50,
+  },
+  {
+    id: 2,
+    value: 100,
+  },
+  {
+    id: 3,
+    value: 200,
+  },
+  {
+    id: 4,
+    value: 500,
+  },
+]
 
-  const onReduce = value1 => {
-    balance = balance - value1
-    return balance
+class CashWithdrawal extends Component {
+  state = {balance: 2000, denominationsList: initialDenominationsList}
+
+  onReduce = id => {
+    for (const each of initialDenominationsList) {
+      if (each.id === id) {
+        this.setState(prevState => ({
+          balance: prevState - each.value,
+        }))
+        break
+      }
+    }
   }
 
-  return (
-    <div className="bg-container1">
-      <div className="bg-container2">
-        <div className="user-name">
-          <div className="profile">S</div>
-          <p>Sarah Williams </p>
-        </div>
-        <div className="balance">
-          <p className="your-balance">Your Balance</p>
-          <div className="amount">
-            <h1>{balance}</h1>
-            <p className="your-balance">In Rupees</p>
+  render() {
+    const {balance} = this.state
+    return (
+      <div className="bg-container1">
+        <div className="bg-container2">
+          <div className="user-name">
+            <div className="profile">S</div>
+            <p>Sarah Williams </p>
           </div>
+          <div className="balance">
+            <p className="your-balance">Your Balance</p>
+            <div className="amount">
+              <p className="acc-bal">{balance}</p>
+              <p className="your-balance">In Rupees</p>
+            </div>
+          </div>
+          <p className="acc-bal">Withdraw</p>
+          <p className="your-balance">CHOOSE SUM (IN RUPEES)</p>
+          <ul>
+            {initialDenominationsList.map(eachItem => (
+              <DenominationItem
+                eachItem={eachItem}
+                key={eachItem.id}
+                onReduce={this.onReduce}
+              />
+            ))}
+          </ul>
         </div>
-        <h1>Withdraw</h1>
-        <p className="your-balance">CHOOSE SUM (IN RUPEES)</p>
-        <ul>
-          {denominationsList.map(eachItem => (
-            <DenominationItem
-              eachItem={eachItem}
-              key={eachItem.id}
-              onReduce={onReduce}
-            />
-          ))}
-        </ul>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default CashWithdrawal
